fb85a5e67ec460a045b333d6c517824b07638ea8
vinhtruongfpt-hue <vinhtruongfpt@gmail.com>
2025-08-17 16:36:04 +0700

    Add server.js by Architect

---
+++ server.js
@@ -0,0 +1,21 @@
+```javascript
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Sample route
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'API is working!' });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+```
