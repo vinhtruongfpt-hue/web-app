2c0f13f01e41b1b17795cde3a0cf06b23e525d89
vinhtruongfpt-hue <vinhtruongfpt@gmail.com>
2025-08-17 17:08:53 +0700

    Add client/src/App.js by Architect

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+```javascript
+import React from 'react';
+import PullBranch from './components/PullBranch';
+
+function App() {
+    return (
+        <div className="App">
+            <h1 className="text-2xl font-bold">Dev AI Tool</h1>
+            <PullBranch />
+        </div>
+    );
+}
+
+export default App;
+```
