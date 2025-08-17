15da46b16121af812e0c4e5b737722b58181fa6d
vinhtruongfpt-hue <vinhtruongfpt@gmail.com>
2025-08-17 16:29:45 +0700

    Add client/src/App.js by Architect

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Quizlet Clone</h1>
+      <p>Welcome to the Quizlet-like Application!</p>
+    </div>
+  );
+}
+
+export default App;
