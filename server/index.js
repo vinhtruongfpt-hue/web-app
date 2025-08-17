b5182d1e0dd372b0945583125fa7173a7a1f4a25
vinhtruongfpt-hue <vinhtruongfpt@gmail.com>
2025-08-17 16:29:41 +0700

    Add server/index.js by Architect

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Basic route for testing
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
